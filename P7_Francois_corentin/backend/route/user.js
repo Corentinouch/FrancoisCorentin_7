99afee83de88f350c7145a092beb9146f8ce38f8
Corentinouch <79088605+Corentinouch@users.noreply.github.com>
2022-05-24 10:05:11 +0200

    Delete P7_Francois_corentin directory

--- P7_Francois_corentin/backend/route/user.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userCtrl = require("../controller/user");
-
-router.get("/:id",userCtrl.getUser)
-router.post("/", userCtrl.signup)
-
-module.exports = router;
